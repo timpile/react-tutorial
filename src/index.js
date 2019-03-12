bdb37b73113115f5b0aab4816fa18a472e39cfa4
timpile <tpile@automatedinsights.com>
2019-03-12 11:41:17 -0400

    Turn square class into a function

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -25,7 +19,8 @@ class Board extends React.Component {
   }
 
   handleClick(i) {
-    const squares = this.state.squares.slice();
+    // const squares = this.state.squares.slice();
+    const squares = {...this.state.squares}; // using spread syntax
     squares[i] = 'X';
     this.setState({squares: squares});
   }
